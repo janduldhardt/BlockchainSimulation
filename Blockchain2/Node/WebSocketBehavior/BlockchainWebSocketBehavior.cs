c55e1b98d070399130a9aa4bca58d461fbe0f8e1
Jan Duldhardt <duldhardtjan@gmail.com>
2021-01-23 16:05:01 +0700

    Connection 1.0

---
+++ Blockchain2/Node/WebSocketBehavior/BlockchainWebSocketBehavior.cs
@@ -0,0 +1,24 @@
+﻿using WebSocketSharp;
+
+namespace Blockchain2.Node.WebSocketBehavior
+{
+    public class BlockchainWebSocketBehavior : WebSocketSharp.Server.WebSocketBehavior
+    {
+        private Node _node;
+
+        public BlockchainWebSocketBehavior() : this(null)
+        {
+        }
+        
+        public BlockchainWebSocketBehavior(Node node)
+        {
+            _node = node;
+        }
+
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            
+            
+        }
+    }
+}
--- Blockchain2/Program.cs
+++ Blockchain2/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using Newtonsoft.Json;
 
 namespace Blockchain2
 {
@@ -6,7 +9,69 @@ namespace Blockchain2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int port = FreeTcpPort();
+            string name = "Unknown";
+
+            if (args.Length >= 1)
+                port = int.Parse(args[0]);
+            if (args.Length >= 2)
+                name = args[1];
+
+            if (name != "Unkown")
+            {
+                Console.WriteLine($"Current user is {name}");
+            }
+
+            var node = new Node.Node(port);
+
+            Console.WriteLine("=========================");
+            Console.WriteLine("1. Connect to a server");
+            Console.WriteLine("2. Add a transaction");
+            Console.WriteLine("3. Display Blockchain");
+            Console.WriteLine("4. Exit");
+            Console.WriteLine("=========================");
+
+            int selection = 0;
+            while (selection != 4)
+            {
+                switch (selection)
+                {
+                    case 1:
+                        Console.WriteLine("Please enter the server URL");
+                        string serverURL = Console.ReadLine();
+                        node.Connect($"{serverURL}");
+                        break;
+                    case 2:
+                        // Console.WriteLine("Please enter the receiver name");
+                        // string receiverName = Console.ReadLine();
+                        // Console.WriteLine("Please enter the amount");
+                        // string amount = Console.ReadLine();
+                        // PhillyCoin.CreateTransaction(new Transaction(name, receiverName, int.Parse(amount)));
+                        // PhillyCoin.ProcessPendingTransactions(name);
+                        // Client.Broadcast(JsonConvert.SerializeObject(PhillyCoin));
+                        break;
+                    case 3:
+                        Console.WriteLine("Blockchain");
+                        Console.WriteLine(JsonConvert.SerializeObject(node.MyBlockchain, Formatting.Indented));
+                        break;
+                }
+
+                Console.WriteLine("Please select an action");
+                string action = Console.ReadLine();
+                selection = int.Parse(action);
+            }
+
+            node.Close();
+        }
+
+
+        static int FreeTcpPort()
+        {
+            TcpListener l = new TcpListener(IPAddress.Loopback, 0);
+            l.Start();
+            int port = ((IPEndPoint) l.LocalEndpoint).Port;
+            l.Stop();
+            return port;
         }
     }
 }
